c2b78d07509c83ff3d9b1e5d5a9b33c8af2d0b9c
nazmulbasher <nazmul.basher@fieldnation.com>
2019-12-14 13:23:03 +0600

    added codes

---
+++ learning/variables.js
@@ -0,0 +1,8 @@
+// Array
+var color = ["Red", "Green", "Blue"];
+var list = [1, 2, 3];
+console.dir(color);
+// Tuple
+var x = ["Nazmul", 37];
+var y = ["Nazmul", 37, "Nabil", 10];
+console.dir(y);
---
+++ learning/variables.ts
@@ -0,0 +1,12 @@
+
+// Array
+let color: string[] = ["Red", "Green", "Blue"];
+let list: number[] = [1, 2, 3];
+
+console.dir(color);
+
+// Tuple
+let x: [string, number] = ["Nazmul", 37];
+let y: [string, number, string, number] = ["Nazmul", 37, "Nabil", 10];
+
+console.dir(y);
