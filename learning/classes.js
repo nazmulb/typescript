8e7bd7d431f6d22d8c3d41ae6c95a1cb49b40e90
nazmulbasher <nazmul.basher@fieldnation.com>
2019-12-17 11:21:40 +0600

    added codes

--- learning/classes.js
+++ learning/classes.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Greeter = /** @class */ (function () {
     function Greeter(message) {
         this.greeting = message;
@@ -9,3 +22,64 @@ var Greeter = /** @class */ (function () {
 }());
 var g = new Greeter("Nazmul");
 console.log(g.greet());
+// Inheritance
+var Animal = /** @class */ (function () {
+    function Animal(theName) {
+        this.name = theName;
+    }
+    Animal.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 0; }
+        console.log(this.name + " moved " + distanceInMeters + "m.");
+    };
+    return Animal;
+}());
+var Snake = /** @class */ (function (_super) {
+    __extends(Snake, _super);
+    function Snake() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Snake.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 5; }
+        console.log("Slithering...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Snake;
+}(Animal));
+var Horse = /** @class */ (function (_super) {
+    __extends(Horse, _super);
+    function Horse() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Horse.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 45; }
+        console.log("Galloping...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Horse;
+}(Animal));
+var sam = new Snake("Sammy the Python");
+var tom = new Horse("Tommy the Palomino");
+sam.move();
+tom.move(34);
+// Understanding protected
+var Man = /** @class */ (function () {
+    function Man(name) {
+        this.name = name;
+    }
+    return Man;
+}());
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(name, department) {
+        var _this = _super.call(this, name) || this;
+        _this.department = department;
+        return _this;
+    }
+    Employee.prototype.getElevatorPitch = function () {
+        return "Hello, my name is " + this.name + " and I work in " + this.department + ".";
+    };
+    return Employee;
+}(Man));
+var emp = new Employee("Nazmul", "Software Engineering");
+console.log(emp.getElevatorPitch());
+// console.log(emp.name); // error
--- learning/classes.ts
+++ learning/classes.ts
@@ -12,3 +12,60 @@ class Greeter {
 
 let g = new Greeter("Nazmul");
 console.log(g.greet());
+
+// Inheritance
+class Animal {
+    private name: string;
+
+    constructor(theName: string) {
+        this.name = theName;
+    }
+
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+class Snake extends Animal {
+    move(distanceInMeters = 5) {
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+
+class Horse extends Animal {
+    move(distanceInMeters = 45) {
+        console.log("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+
+const sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+sam.move();
+tom.move(34);
+
+// Understanding protected
+class Man {
+    protected name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+class Employee extends Man {
+    private department: string;
+
+    constructor(name: string, department: string) {
+        super(name);
+        this.department = department;
+    }
+
+    public getElevatorPitch() {
+        return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+    }
+}
+
+let emp = new Employee("Nazmul", "Software Engineering");
+console.log(emp.getElevatorPitch());
+// console.log(emp.name); // error
