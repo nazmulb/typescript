c018fde1c2fbf5cd06aae6406a10d8fecd5cd9e7
nazmulbasher <nazmul.basher@fieldnation.com>
2019-12-18 14:43:27 +0600

    added codes

---
+++ learning/symbols.js
@@ -0,0 +1,9 @@
+// Symbols are immutable, and unique.
+let sym2 = Symbol("key");
+let sym3 = Symbol("key");
+console.log(sym2 === sym3);
+const sym = Symbol();
+let obj2 = {
+    [sym]: "Nazmul"
+};
+console.log(obj2[sym]);
